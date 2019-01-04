1d35411184e93c322a5ba44db44e38c9a037b122
unknown <dennis.alonzo.johnson@gmail.com>
2019-01-04 18:26:29 -0500

    first commit

---
+++ mail.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data:{
+    product: 'Red Shoes',
+    description: 'Red Baller Shoes',
+    image: 'https://resizing.flixster.com/PamuH4K1SFI5ULuzoo0LobR3kxs=/300x300/v1.bjs2ODYyMTk7ajsxNzkxNzsxMjAwOzE5MjA7MTA4MA',
+    shoeUrl: 'https://www.tangeroutlet.com/daytona',
+    inventory: 0,
+    onSale: false
+
+  }
+})
