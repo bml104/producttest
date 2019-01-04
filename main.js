c1375ea070c954b0c3484ae7c059195b48d3e19a
unknown <dennis.alonzo.johnson@gmail.com>
2019-01-04 18:32:26 -0500

    #Added Details

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var app = new Vue({
   el: '#app',
   data:{
-    product: 'Red Shoes',
+    product: 'Red Shoe',
     description: 'Red Baller Shoes',
     image: 'https://resizing.flixster.com/PamuH4K1SFI5ULuzoo0LobR3kxs=/300x300/v1.bjs2ODYyMTk7ajsxNzkxNzsxMjAwOzE5MjA7MTA4MA',
     shoeUrl: 'https://www.tangeroutlet.com/daytona',
-    inventory: 0,
-    onSale: false
+    inventory: 32,
+    onSale: false,
+    details: ["cotton", "red", "low top"]
 
   }
 })
